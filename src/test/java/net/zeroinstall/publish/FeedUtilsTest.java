a6d685bbe3b5cab97d2a45457164a543c9b404d5
Bastian Eicher <bastian@eicher.net>
2013-05-13 17:10:16 +0200

    Added FeedUtils and GnuPG with unit test
    --HG--
    extra : amend_source : 60833b8abf99f7a2fd993d74c7495ac631916f93

---
+++ src/test/java/net/zeroinstall/publish/FeedUtilsTest.java
@@ -0,0 +1,37 @@
+package net.zeroinstall.publish;
+
+import net.zeroinstall.model.InterfaceDocument;
+import static net.zeroinstall.publish.FeedUtils.*;
+import org.apache.xmlbeans.XmlException;
+import org.junit.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class FeedUtilsTest {
+
+    @Test
+    public void xmlPrelude() {
+        InterfaceDocument feed = InterfaceDocument.Factory.newInstance();
+        feed.addNewInterface();
+
+        assertThat(toXmlText(feed), startsWith("<?xml"));
+    }
+
+    @Test
+    public void xmlRoundtrip() throws XmlException {
+        InterfaceDocument feed = InterfaceDocument.Factory.newInstance();
+        feed.addNewInterface().addName("Test Name");
+        feed = InterfaceDocument.Factory.parse(toXmlText(feed));
+
+        assertThat(feed.getInterface().getNameArray(0), equalTo("Test Name"));
+    }
+
+    @Test
+    public void stylesheet() {
+        InterfaceDocument feed = InterfaceDocument.Factory.newInstance();
+        feed.addNewInterface();
+        addStylesheet(feed);
+
+        assertThat(toXmlText(feed), containsString("<?xml-stylesheet type='text/xsl' href='interface.xsl'?>"));
+    }
+}
