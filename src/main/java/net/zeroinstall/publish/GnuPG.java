4575f76c288bb7e2c5c3bb5e8691f5d49b3687e3
Bastian Eicher <bastian@eicher.net>
2013-05-17 12:06:06 +0200

    Refactored GnuPG

--- src/main/java/net/zeroinstall/publish/GnuPG.java
+++ src/main/java/net/zeroinstall/publish/GnuPG.java
@@ -15,38 +15,33 @@ public final class GnuPG {
     }
 
     public static String getPublicKey(String keySpecifier) throws IOException {
-        try {
-            Process process = new ProcessBuilder(
-                    "gpg", "-a", "--export", checkNotNull(keySpecifier)).
-                    start();
+        Process process = new ProcessBuilder(
+                "gpg", "-a", "--export", checkNotNull(keySpecifier)).
+                start();
+        waitForExit(process);
 
-            if (process.waitFor() != 0) {
-                String errorMessage = new Scanner(process.getErrorStream()).useDelimiter("\\A").next();
-                throw new IOException(errorMessage);
-            }
-
-            return new Scanner(process.getInputStream()).useDelimiter("\\A").next();
-        } catch (InterruptedException ex) {
-            throw new IOException(ex);
-        }
+        return new Scanner(process.getInputStream()).useDelimiter("\\A").next();
     }
 
     public static byte[] detachSign(String data, String keySpecifier) throws IOException {
+        Process process = new ProcessBuilder(
+                "gpg", "--detach-sign", "--default-key", checkNotNull(keySpecifier), "--output", "-", "-").
+                start();
+
+        PrintWriter writer = new PrintWriter(process.getOutputStream());
+        writer.write(data);
+        writer.close();
+        waitForExit(process);
+
+        return toByteArray(process.getInputStream());
+    }
+
+    private static void waitForExit(Process process) throws IOException {
         try {
-            Process process = new ProcessBuilder(
-                    "gpg", "--detach-sign", "--default-key", checkNotNull(keySpecifier), "--output", "-", "-").
-                    start();
-
-            PrintWriter writer = new PrintWriter(process.getOutputStream());
-            writer.write(data);
-            writer.close();
-
             if (process.waitFor() != 0) {
                 String errorMessage = new Scanner(process.getErrorStream()).useDelimiter("\\A").next();
                 throw new IOException(errorMessage);
             }
-
-            return toByteArray(process.getInputStream());
         } catch (InterruptedException ex) {
             throw new IOException(ex);
         }
