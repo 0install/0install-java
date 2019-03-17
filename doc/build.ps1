ed34b1e7bb36ddb5b289bd725f1ec8108cf00a9a
Bastian Eicher <bastian@eicher.net>
2019-03-17 17:15:26 +0100

    Fixed API doc

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-# Ensure 0install is in the PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
+if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { # Ensure 0install is in PATH
+    echo "Downloading 0install"
     mkdir -Force "$env:TEMP\zero-install" | Out-Null
     Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
